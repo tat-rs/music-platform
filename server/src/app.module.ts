6465b940065284af936466ee47c7c649a2a93bef
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-12-13 22:56:28 +0300

    feat: раздача статики с сервера

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin123321@cluster0.dzmxpag.mongodb.net/?retryWrites=true&w=majority',
     ),
