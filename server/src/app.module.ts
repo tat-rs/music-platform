a769c47bf96c3e59d790afa364c700f727d5ae88
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-12-15 22:18:41 +0300

    fix: mongodb url

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
+import * as dotenv from 'dotenv';
 import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 
+/* dotenv.config(); */
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin123321@cluster0.dzmxpag.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    /* MongooseModule.forRoot(process.env.MONGODB_URL), */
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     TrackModule,
     FileModule,
   ],
