a53ba5fd3e87287025c9488537236a3c1787ba87
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-12-24 18:17:05 +0300

    feat: создать форму добавления обложки

---
+++ frontend/components/FormAddCover.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "../styles/FormAddCover.module.scss";
+
+function FormAddCover() {
+
+  return (
+    <div className={styles.cover}>
+      <Image
+        src='https://images.unsplash.com/photo-1669666808012-3e120637a62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        alt="Обложка"
+        width={244}
+        height={198}
+        className={styles.cover__image}
+      />
+      <div className={styles.cover__container}>
+        <h3 className={styles.cover__title}>Загрузите обложку трека</h3>
+        <input
+          className={styles.cover__input}
+          id="cover"
+          name="cover"
+          type="file" />
+        <label className={styles.cover__label} htmlFor="cover">
+          <span className={styles.cover__button} >Загрузить</span>
+        </label>
+      </div>
+    </div>
+  )
+}
+
+export default FormAddCover;
