00e9a47da5b8b6ec2c886204123f54c900d21fc9
Tatiana Roslyakova <tat-rs@yandex.ru>
2023-01-06 23:49:40 +0300

    fix: исправить поиск треков

--- frontend/pages/tracks.tsx
+++ frontend/pages/tracks.tsx
@@ -1,11 +1,45 @@
 import { Context } from "next-redux-wrapper";
+import { useState } from "react";
+import LinkElement from "../components/Link";
 import TrackList from "../components/TrackList";
-import { useAppSelector } from "../hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { NextThunkDispatch, wrapper } from "../store/store";
-import { fetchTracks } from "../store/tracks/thunk";
+import { fetchTracks, searchTracks } from "../store/tracks/thunk";
+import { categoryList } from "../utils/constants";
+
+import styles from "../styles/Tracks.module.scss";
 
 export default function Tracks() {
   const {tracks, isLoading, isError} = useAppSelector((state) => state.tracks);
+  const [searchValue, setSearchValue] = useState('');
+  const [category, setCategory] = useState(categoryList[0]);
+  const dispatch = useAppDispatch();
+  const [timer, setTimer] = useState<any>(null);
+
+  async function search(evt: React.ChangeEvent<HTMLInputElement>) {
+    evt.preventDefault();
+    setSearchValue(evt.target.value);
+    if(timer) {
+      clearTimeout(timer)
+    }
+    setTimer(
+      setTimeout(async() => {
+        if(evt.target.value !== "") {
+          await dispatch(searchTracks({category: category.value, query: evt.target.value}));
+        } else {
+          await dispatch(fetchTracks());
+        }
+      }, 1000)
+    )
+  }
+
+  function changeCategory(evt: React.ChangeEvent<HTMLSelectElement>) {
+    evt.preventDefault();
+    setCategory({
+      name: evt.target.options[evt.target.selectedIndex].text,
+      value: evt.target.value
+    });
+  }
 
   if(isLoading) {
     return (
@@ -21,9 +55,46 @@ export default function Tracks() {
 
   return (
     <>
+      <section className={styles.tracks}>
+      <div className={styles.tracks__heading}>
+        <h2 className={styles.tracks__title}>Список треков</h2>
+        <LinkElement link="/tracks/create" text="Загрузить" />
+      </div>
+      <div className={styles.tracks__search}>
+        <label htmlFor="search" className={styles.tracks__query}>
+          <input
+            className={styles.tracks__input}
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Поиск..."
+            value={searchValue}
+            onChange={(evt: React.ChangeEvent<HTMLInputElement>) => search(evt)} />
+        </label>
+        <label htmlFor="category" className={styles.tracks__select}>
+          <select
+            className={styles.tracks__input}
+            name="category"
+            id="category"
+            value={category.value}
+            onChange={(evt: React.ChangeEvent<HTMLSelectElement>) => changeCategory(evt)}
+            >
+            {
+              categoryList.map((el, i) => (
+                <option key={i} value={el.value}>{el.name}</option>
+              ))
+            }
+          </select>
+        </label>
+      </div>
       {
-        tracks?.length > 0 && <TrackList tracks={tracks}/>
+        tracks?.length > 0 ? (
+          <TrackList tracks={tracks} />
+        ) : (
+          <p className={styles.tracks__answer}>Треков нет...</p>
+        )
       }
+    </section>
     </>
   )
 }
